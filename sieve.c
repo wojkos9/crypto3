5dd360f694a9b66411734e0a2103d1ac174014d0
wojkos9 <wojkos9@gmail.com>
2022-03-08 14:52:40 +0100

    Togglable tests

--- sieve.c
+++ sieve.c
@@ -35,7 +35,6 @@ void primes_under(int n) {
   }
 
   printf("Primes <= %d: %d\n", n, n_primes);
-
   free(sieve);
 }
 
@@ -56,11 +55,11 @@ void mersenne_primes(int n) {
   uint8_t *sieve = mksieve(n);
 
   int n_primes = 0;
-  int x = 1;
-  int e = 0;
+  int e = 2;
+  int x = 4; // 2^e
   while(x < n) {
     int p = x-1;
-    if (!sieve[p]) {
+    if (!sieve[p]) { // if e is composite, so is p = 2^e - 1, so this is enough
       ++n_primes;
       dbg(2, "Mersenne prime %d (2^%d-1)\n", p, e);
     }
@@ -72,12 +71,27 @@ void mersenne_primes(int n) {
   free(sieve);
 }
 
+#define DO_TWINS 1
+#define DO_MERSENNE 2
+#define DO_LIST 4
+
+int tests = 7;
+
 void parse_opts(int argc, char *argv[]) {
   char c;
-  while ((c = getopt(argc, argv, "v")) > 0) {
+  while ((c = getopt(argc, argv, "vtml")) > 0) {
     switch(c) {
       case 'v':
         ++LOGLEVEL;
+        break;
+      case 't':
+        tests ^= DO_TWINS;
+        break;
+      case 'm':
+        tests ^= DO_MERSENNE;
+        break;
+      case 'l':
+        tests ^= DO_LIST;
         break;
     }
   }
@@ -85,9 +99,9 @@ void parse_opts(int argc, char *argv[]) {
 
 int main (int argc, char *argv[]) {
   parse_opts(argc, argv);
-  primes_under(25);
-  twin_primes(0, 2137);
-  mersenne_primes(10000000);
-  primes_under(10000);
+  // primes_under(25);
+  if (tests & DO_TWINS)     twin_primes(0, 2137);
+  if (tests & DO_MERSENNE)  mersenne_primes(10000000);
+  if (tests & DO_LIST)      primes_under(10000);
   return 0;
 }
